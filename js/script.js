40087ba5662379796761c76b891d7e704face6ce
FELIPE ORTIGOZA <0200832311037>
2024-11-07 10:53:51 -0300

    Front atualizado

--- js/script.js
+++ js/script.js
@@ -23,31 +23,3 @@ prev.onclick = function () {
   active = (active - 1 + items.length) % items.length; // Move para o slide anterior
   loadShow();
 };
-
-// script do formulario
-
-function validarFormulario() {
-  let email = document.getElementById("email");
-  let senha = document.getElementById("senha");
-  let erro = false;
-
-  // Limpar mensagens de erro antes de validação
-  document.getElementById("erroEmail").textContent = "";
-  document.getElementById("erroSenha").textContent = "";
-
-  // Verificar campo de email
-  if (email.value.trim() === "") {
-    document.getElementById("erroEmail").textContent = "Preencha o email";
-    erro = true;
-  }
-
-  // Verificar campo de senha
-  if (senha.value.trim() === "") {
-    document.getElementById("erroSenha").textContent = "Preencha a senha";
-    erro = true;
-  }
-
-  // Se houver erro, não submeter o formulário
-  return !erro;
-}
-
